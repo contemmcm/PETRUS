5c950f3454a03d6245354c9ab4c322e129331ac6
Marcio Conte <marcio@letrus.com.br>
2018-11-18 20:34:00 -0200

    Updating setuptools.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(
-    name='petrus',
-    version='0.1',
-    description='Online automatic phonetic transcription system for Brazilian Portuguese',
-    author='Alessandro Bokan',
-    url='https://github.com/contemmcm/PETRUS',
-    py_modules=['petrus']
-)
+setup(name='petrus',
+      version='0.1',
+      description='Online automatic phonetic transcription system for Brazilian Portuguese',
+      url='https://github.com/contemmcm/PETRUS',
+      author='Alessandro Bokan',
+      author_email='abokan@gmail.com',
+      packages=['petrus', 'petrus.g2p', 'petrus.stress', 'petrus.syllables'],
+      zip_safe=False)
